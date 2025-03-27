96d13143182047ea5205a303585f05d5d9e69d23
Cam Shinker <shinkecj@mail.uc.edu>
2025-03-27 09:33:57 -0400

    Add project files.

---
+++ KentuckyWildcats_Assignment09/database_package/database_management.py
@@ -0,0 +1,13 @@
+# File Name: database_management.py
+# Student Name: Cam Shinker, Vanshika Rana
+# email: shinkecj@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 4/3/2025
+# Course #/Section: IS 4010-002
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Connect to a database and complete some tasks
+
+# Brief Description of what this module does: Contains classes that complete the assignment
+# Citations: 
+
+# Anything else that's relevant:
---
+++ KentuckyWildcats_Assignment09/main_package/Main.py
@@ -0,0 +1,17 @@
+# File Name: main.py
+# Student Name: Cam Shinker
+# email: shinkecj@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 4/3/2025
+# Course #/Section: IS 4010-002
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Connect to a database and complete some tasks
+
+# Brief Description of what this module does: Contains the code that completes the assignment
+# Citations: 
+
+# Anything else that's relevant:
+
+
+if __name__ == "__main__":
+    pass
