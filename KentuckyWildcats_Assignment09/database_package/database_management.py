a295872f0792ee640bc661a9595062c941d2b7eb
Vanshika Rana <ranava@mail.uc.edu>
2025-03-30 20:09:00 -0400

    Step 1 and step 2.

--- KentuckyWildcats_Assignment09/database_package/database_management.py
+++ KentuckyWildcats_Assignment09/database_package/database_management.py
@@ -1,4 +1,4 @@
-# File Name: database_management.py
+﻿# File Name: database_management.py
 # Student Name: Cam Shinker, Vanshika Rana
 # email: shinkecj@mail.uc.edu
 # Assignment Number: Assignment 09
@@ -11,3 +11,53 @@
 # Citations: 
 
 # Anything else that's relevant:
+
+import pyodbc
+import random
+
+
+class Product:
+    def __init__(self, product_id, upc, description, manufacturer_id, brand_id):
+        self.product_id = product_id
+        self.upc = upc
+        self.description = description.strip()
+        self.manufacturer_id = manufacturer_id
+        self.brand_id = brand_id
+
+    def describe(self):
+        return (
+            f"The product '{self.description}' (Product ID {self.product_id}) is manufactured "
+            f"by company {self.manufacturer_id} and branded under brand {self.brand_id}."
+        )
+
+
+def fetch_products():
+    conn = pyodbc.connect(
+        'Driver={SQL Server};'
+        'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+        'Database=GroceryStoreSimulator;'  
+        'uid=IS4010Login;'
+        'pwd=P@ssword2;'
+    )
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT ProductID, [UPC-A ], Description, ManufacturerID, BrandID FROM tProduct")
+
+    products = []
+
+    for row in cursor.fetchall():
+        product = Product(
+            product_id=row.ProductID,
+            upc=row[1],
+            description=row.Description,
+            manufacturer_id=row.ManufacturerID,
+            brand_id=row.BrandID
+        )
+        products.append(product)
+
+    conn.close()
+    return products
+
+def get_random_product(products):
+    return random.choice(products)
+
